72045022d43a2062a3099a68056a1f4b309142d3
Karen Lopez <stefanny@utp.edu.co>
2021-02-01 06:40:18 -0500

    feat: Create Roulette Repository

---
+++ src/main/java/com/karen/roulette/domain/repository/RouletteRepository.java
@@ -0,0 +1,29 @@
+package com.karen.roulette.domain.repository;
+
+import com.karen.roulette.persistence.crud.RouletteCrudRepository;
+import com.karen.roulette.persistence.entity.Roulette;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class RouletteRepository {
+    private RouletteCrudRepository rouletteCrudRepository;
+
+    public List<Roulette> getAll(){
+        return rouletteCrudRepository.findAll();
+    }
+
+    public Optional<Roulette> getById(Integer rouletteId){
+        return rouletteCrudRepository.findById(rouletteId);
+    }
+
+    public int setWinningNumber(Integer rouletteId, Integer winningNumber){
+        return rouletteCrudRepository.updateRouletteSetWinningNumberForId(winningNumber, rouletteId);
+    }
+
+    public void setRoulette(boolean available){
+        rouletteCrudRepository.insertRoulette(available);
+    }
+}
---
+++ src/main/java/com/karen/roulette/persistence/crud/RouletteCrudRepository.java
@@ -0,0 +1,25 @@
+package com.karen.roulette.persistence.crud;
+
+import com.karen.roulette.persistence.entity.Roulette;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface RouletteCrudRepository extends CrudRepository<Roulette, Integer> {
+    @Query(value = "SELECT r FROM roulettes r", nativeQuery = true)
+    public List<Roulette> findAll();
+    @Query(value = "SELECT r FROM roulettes r WHERE r.roulette_id = :rouletteId", nativeQuery = true)
+    public Optional<Roulette> findById(@Param("rouletteId") Integer rouletteId);
+    @Modifying
+    @Query(value = "UPDATE roulettes r SET r.winning_number = :winningNumber WHERE r.roulette_id = :rouletteId",
+            nativeQuery = true)
+    int updateRouletteSetWinningNumberForId(@Param("winningNumber") Integer winningNumber, @Param("rouletteId") Integer rouletteId);
+    @Modifying
+    @Query(value ="insert into roulettes (available) values (:available)",
+            nativeQuery = true)
+    void insertRoulette(@Param("available") boolean available);
+}
