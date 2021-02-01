6ab9a74306f6243f7219022d8afab6c1ccafcdac
Karen Lopez <stefanny@utp.edu.co>
2021-02-01 15:11:29 -0500

    create endPoints

--- src/main/java/com/karen/roulette/persistence/crud/BetCrudRepository.java
+++ src/main/java/com/karen/roulette/persistence/crud/BetCrudRepository.java
@@ -16,7 +16,7 @@ public interface BetCrudRepository extends CrudRepository<Bet,Integer> {
     @Query(value = "SELECT b FROM bets b WHERE b.roulette_id = :rouletteId", nativeQuery = true)
     public List<Bet> findByRouletteId(@Param("rouletteId") Integer rouletteId);
     @Modifying
-    @Query(value ="insert into bets (roulette_id, bet_option_id, user_id, money) values (:rouletteId, :betOptionId, userId, money)",
+    @Query(value ="insert into bets (roulette_id, bet_option_id, user_id, money) values (:rouletteId, :betOptionId, :userId, :money)",
             nativeQuery = true)
     void insertBet(@Param("rouletteId") Integer rouletteId, @Param("betOptionId") Integer betOptionId,
                         @Param("userId") String userId, @Param("money") Integer money);
